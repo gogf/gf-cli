265fc1f7d6edabf954ea55917418087e576dfa74
sth4me <414512894@qq.com>
2020-01-06 02:25:11 +0800

    Update install.go try to fix alias
    因为检测当前的alias列表对我而言相当困难，所以仅仅检测当前环境是否为zsh，并尝试修改.zshrc文件添加`alias gf=gf`一行。

--- commands/install/install.go
+++ commands/install/install.go
@@ -16,6 +16,15 @@ func Run() {
 	} else {
 		mlog.Printf("gf binary is successfully installed to: %s", path)
 	}
+	if IsInstalled() {
+		shell := CurrentShell()
+		if shell == "zsh" {
+			err := AliasZSHConf("gf", "gf")
+			if err != nil {
+				mlog.Print("your shell is zsh, you might need rename your alias by command `echo alias gf=gf>>~/.zshrc && source ~/.zshrc` to resolve the conflicts between `gf` and `git fetch`")
+			}
+		}
+	}
 }
 
 // IsInstalled returns whether the binary installed.
@@ -35,4 +44,36 @@ func GetInstallFolderPath() string {
 // getInstallBinaryPath returns the installation path for the binary.
 func GetInstallBinaryPath() string {
 	return gfile.Join(GetInstallFolderPath(), "gf"+gfile.Ext(gfile.SelfPath()))
+}
+
+// currentShell returns current shell name
+func CurrentShell() string {
+	if runtime.GOOS != "windows" {
+		osPid := gproc.PPidOS()
+		psCmd := fmt.Sprintf("ps -p %d -ocomm=", osPid)
+		ps, err := gproc.ShellExec(psCmd)
+		if err != nil {
+			return ""
+		}
+		return gstr.Trim(ps)
+	}
+	return ""
+}
+
+// aliasZSHConf append `alias command=dstCommand` to .zshrc if this file exist
+func AliasZSHConf(command, dstCommand string) error {
+	homePath, err := gfile.Home()
+	if err != nil {
+		return err
+	}
+	confPath := fmt.Sprintf("%s/.zshrc", homePath)
+	if gfile.Exists(confPath) {
+		content := gfile.GetContents(confPath)
+		if !gstr.ContainsI(content, "alias gf=gf") {
+			if err := gfile.PutContentsAppend(confPath, fmt.Sprintf("alias %s=%s", command, dstCommand)); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
 }
