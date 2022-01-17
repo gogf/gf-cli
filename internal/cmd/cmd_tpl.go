a07393b78166b7b0c64f3c5802720901606613e7
John Guo <john@johng.cn>
2022-01-17 16:26:01 +0800

    improve command tpl

--- internal/cmd/cmd_tpl.go
+++ internal/cmd/cmd_tpl.go
@@ -31,6 +31,8 @@ like json/xml/yaml/toml/ini.
 	cTplParseBrief = `parse either template file or folder with multiple types of values`
 	cTplParseEg    = `
 gf tpl parse -p ./template -v values.json -r
+gf tpl parse -p ./template -v values.json -n *.tpl -r
+gf tpl parse -p ./template -v values.json -d ${,}} -r
 gf tpl parse -p ./template -v values.json -o ./template.parsed
 `
 	cTplSupportValuesFilePattern = `*.json,*.xml,*.yaml,*.yml,*.toml,*.ini`
@@ -38,11 +40,14 @@ gf tpl parse -p ./template -v values.json -o ./template.parsed
 
 type (
 	cTplParseInput struct {
-		g.Meta  `name:"parse" brief:"{cTplParseBrief}" eg:"{cTplParseEg}"`
-		Path    string `name:"path"    short:"p" brief:"template file or folder path" v:"required"`
-		Values  string `name:"values"  short:"v" brief:"template values file/folder, support file types like: json/xml/yaml/toml/ini" v:"required"`
-		Output  string `name:"output"  short:"o" brief:"output file/folder path"`
-		Replace bool   `name:"replace" short:"r" brief:"replace original files" orphan:"true"`
+		g.Meta     `name:"parse" brief:"{cTplParseBrief}" eg:"{cTplParseEg}"`
+		Path       string `name:"path"       short:"p" brief:"template file or folder path" v:"required"`
+		Pattern    string `name:"pattern"    short:"n" brief:"template file pattern when path is a folder, default is:*" d:"*"`
+		Recursive  bool   `name:"recursive"  short:"c" brief:"recursively parsing files if path is folder, default is:true" d:"true"`
+		Values     string `name:"values"     short:"v" brief:"template values file/folder, support file types like: json/xml/yaml/toml/ini" v:"required"`
+		Output     string `name:"output"     short:"o" brief:"output file/folder path"`
+		Delimiters string `name:"delimiters" short:"d" brief:"delimiters for template content parsing, default is:{{,}}" d:"{{,}}"`
+		Replace    bool   `name:"replace"    short:"r" brief:"replace original files" orphan:"true"`
 	}
 	cTplParseOutput struct{}
 )
@@ -60,6 +65,12 @@ func (c *cTpl) Parse(ctx context.Context, in cTplParseInput) (out *cTplParseOutp
 	if in.Output == "" && in.Replace == false {
 		return nil, gerror.New(`parameter output and replace should not be both empty`)
 	}
+	mlog.Debug("delimiters:", in.Delimiters)
+	delimiters := gstr.SplitAndTrim(in.Delimiters, ",")
+	if len(delimiters) != 2 {
+		return nil, gerror.Newf(`invalid delimiters: %s`, in.Delimiters)
+	}
+	g.View().SetDelimiters(delimiters[0], delimiters[1])
 	valuesMap, err := c.loadValues(ctx, in.Values)
 	if err != nil {
 		return nil, err
@@ -67,47 +78,48 @@ func (c *cTpl) Parse(ctx context.Context, in cTplParseInput) (out *cTplParseOutp
 	if len(valuesMap) == 0 {
 		return nil, gerror.Newf(`empty values loaded from values file/folder "%s"`, in.Values)
 	}
-	err = c.parsePath(ctx, in.Path, in.Output, valuesMap, in.Replace)
+	err = c.parsePath(ctx, valuesMap, in)
 	if err == nil {
 		mlog.Print("done!")
 	}
 	return
 }
 
-func (c *cTpl) parsePath(ctx context.Context, path string, output string, values g.Map, replace bool) (err error) {
-	if !gfile.Exists(path) {
-		return gerror.Newf(`path "%s" does not exist`, path)
+func (c *cTpl) parsePath(ctx context.Context, values g.Map, in cTplParseInput) (err error) {
+	if !gfile.Exists(in.Path) {
+		return gerror.Newf(`path "%s" does not exist`, in.Path)
 	}
 	var (
+		path         string
 		files        []string
 		relativePath string
 		outputPath   string
 	)
-	path = gfile.RealPath(path)
+	path = gfile.RealPath(in.Path)
 	if gfile.IsDir(path) {
-		files, err = gfile.ScanDirFile(path, "*", true)
+		files, err = gfile.ScanDirFile(path, in.Pattern, in.Recursive)
 		if err != nil {
 			return err
 		}
 		for _, file := range files {
 			relativePath = gstr.Replace(file, path, "")
-			if output != "" {
-				outputPath = gfile.Join(output, relativePath)
+			if in.Output != "" {
+				outputPath = gfile.Join(in.Output, relativePath)
 			}
-			if err = c.parseFile(ctx, file, outputPath, values, replace); err != nil {
+			if err = c.parseFile(ctx, file, outputPath, values, in); err != nil {
 				return
 			}
 		}
 		return
 	}
-	if output != "" {
-		outputPath = output
+	if in.Output != "" {
+		outputPath = in.Output
 	}
-	err = c.parseFile(ctx, path, outputPath, values, replace)
+	err = c.parseFile(ctx, path, outputPath, values, in)
 	return
 }
 
-func (c *cTpl) parseFile(ctx context.Context, file string, output string, values g.Map, replace bool) (err error) {
+func (c *cTpl) parseFile(ctx context.Context, file string, output string, values g.Map, in cTplParseInput) (err error) {
 	output = gstr.ReplaceByMap(output, g.MapStrStr{
 		`\\`: `\`,
 		`//`: `/`,
@@ -120,7 +132,7 @@ func (c *cTpl) parseFile(ctx context.Context, file string, output string, values
 		mlog.Printf(`parse file "%s" to "%s"`, file, output)
 		return gfile.PutContents(output, content)
 	}
-	if replace {
+	if in.Replace {
 		mlog.Printf(`parse and replace file "%s"`, file)
 		return gfile.PutContents(file, content)
 	}
